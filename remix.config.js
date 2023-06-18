298c6336f7dc6a29524cf47a7a402ec1f91b4492
Maksim Mrug <80621719+kruzhambus@users.noreply.github.com>
2023-06-18 21:33:52 +0300

    netlify deploy remix fix

--- remix.config.js
+++ remix.config.js
@@ -5,6 +5,11 @@ module.exports = {
   // assetsBuildDirectory: "public/build",
   // serverBuildPath: "build/index.js",
   // publicPath: "/build/",
+  server:
+  process.env.NETLIFY || process.env.NETLIFY_LOCAL
+    ? './server.ts'
+    : undefined,
+  serverBuildPath: '.netlify/functions-internal/server.js',
   tailwind: true,
   postcss: true,
   serverModuleFormat: 'cjs',
