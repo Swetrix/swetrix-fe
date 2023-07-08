697ecec1fc5296ae7290d5c5c8c13b63b060ae43
Maksim Mrug <80621719+kruzhambus@users.noreply.github.com>
2023-07-08 22:40:54 +0300

    add funnels chart settings

--- app/pages/Project/View/ViewProject.helpers.tsx
+++ app/pages/Project/View/ViewProject.helpers.tsx
@@ -905,6 +905,108 @@ const getSettingsPerf = (
   }
 }
 
+const getFunnelsSettings = (
+  chart: {
+    [key: string]: string[]
+  },
+  timeBucket: string,
+  chartType: string,
+  timeFormat: string,
+  rotateXAxias: boolean,
+) => {
+  const xAxisSize = _size(chart.x)
+
+  return {
+    data: {
+      x: 'x',
+      columns: [
+        ['x', ..._map(chart.x, el => dayjs(el).toDate())],
+        ['funnels', ...chart.funnels],
+      ],
+      types: {
+        funnels: chartType === chartTypes.line ? area() : bar(),
+      },
+      colors: {
+        funnels: '#2563EB',
+      },
+    },
+    axis: {
+      x: {
+        clipPath: false,
+        tick: {
+          fit: true,
+          rotate: rotateXAxias ? 45 : 0,
+          format: timeFormat === TimeFormat['24-hour'] ? (x: string) => d3.timeFormat(tbsFormatMapper24h[timeBucket])(x) : (x: string) => d3.timeFormat(tbsFormatMapper[timeBucket])(x),
+        },
+        localtime: timeFormat === TimeFormat['24-hour'],
+        type: 'timeseries',
+      },
+      y: {
+        tick: {
+          format: (d: string) => getStringFromTime(getTimeFromSeconds(d), true),
+        },
+      },
+    },
+    transition: {
+      duration: 500,
+    },
+    resize: {
+      auto: true,
+      timer: false,
+    },
+    tooltip: {
+      contents: (item: any, _: any, __: any, color: any) => {
+        return `<ul class='bg-gray-100 dark:text-gray-50 dark:bg-slate-800 rounded-md shadow-md px-3 py-1'>
+        <li class='font-semibold'>${timeFormat === TimeFormat['24-hour'] ? d3.timeFormat(tbsFormatMapperTooltip24h[timeBucket])(item[0].x) : d3.timeFormat(tbsFormatMapperTooltip[timeBucket])(item[0].x)
+}</li>
+        <hr class='border-gray-200 dark:border-gray-600' />
+        ${_map(item, (el: {
+          id: string,
+          index: number,
+          name: string,
+          value: string,
+          x: Date,
+        }) => {
+    return `
+          <li class='flex justify-between'>
+            <div class='flex justify-items-start'>
+              <div class='w-3 h-3 rounded-sm mt-1.5 mr-2' style=background-color:${color(el.id)}></div>
+              <span>${el.name}</span>
+            </div>
+            <span class='pl-4'>${getStringFromTime(getTimeFromSeconds(el.value), true)}</span>
+          </li>
+          `
+  }).join('')}`
+      },
+    },
+    point: {
+      focus: {
+        only: xAxisSize > 1,
+      },
+      pattern: [
+        'circle',
+      ],
+      r: 3,
+    },
+    legend: {
+      usePoint: true,
+      item: {
+        tile: {
+          width: 10,
+        },
+      },
+      hide: perfomanceChartCompare,
+    },
+    area: {
+      linearGradient: true,
+    },
+    padding: {
+      right: rotateXAxias && 35,
+    },
+    bindto: '#dataChart',
+  }
+}
+
 const validTimeBacket = ['hour', 'day', 'week', 'month']
 const validPeriods = ['custom', 'today', 'yesterday', '1d', '7d', '4w', '3M', '12M', '24M']
 const validFilters = ['cc', 'rg', 'ct', 'pg', 'lc', 'ref', 'dv', 'br', 'os', 'so', 'me', 'ca', 'ev']
