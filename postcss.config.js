af3c85e657e281368daf3fa289f156ae532f634e
Maksim Mrug <80621719+kruzhambus@users.noreply.github.com>
2023-08-03 14:41:49 +0300

    (optimizaion) css

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,14 @@
+const purgecss = require('@fullhuman/postcss-purgecss')({
+  content: ['./src/**/*.html', './src/**/*.jsx', './src/**/*.tsx', './src/**/*.js', './src/**/*.ts'],
+})
+const cssnano = require('cssnano')({
+  preset: 'default',
+})
+
 module.exports = {
   plugins: {
     tailwindcss: {},
     autoprefixer: {},
+    ...(process.env.NODE_ENV === 'production' ? [purgecss, cssnano] : []),
   },
 }
