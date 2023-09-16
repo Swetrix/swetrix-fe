0d077b45ba6699a6a1ad4eaf612d28beec8f8228
Blue Mouse <durexp@protonmail.ch>
2023-09-16 09:57:17 +0100

    added tabs logic for user settings & preparing referral program tab

---
+++ app/pages/UserSettings/components/Referral.tsx
@@ -0,0 +1,32 @@
+import React, { memo } from 'react'
+import { useTranslation } from 'react-i18next'
+
+import { IUser } from 'redux/models/IUser'
+
+interface ISocialisations {
+  user: IUser,
+  genericError: (message: string) => void,
+  theme: string
+}
+
+const Socialisations = ({
+  user, genericError, theme,
+}: ISocialisations) => {
+  const { t }: {
+    t: (key: string) => string,
+  } = useTranslation('common')
+
+
+  return (
+    <>
+      <p className='max-w-prose text-base text-gray-900 dark:text-gray-50'>
+        {t('profileSettings.socialisationsDesc')}
+      </p>
+      <div className='overflow-hidden bg-white dark:bg-slate-800 mt-2 shadow sm:rounded-md'>
+        referral program
+      </div>
+    </>
+  )
+}
+
+export default memo(Socialisations)
