7228e1e5a6fa26a108893738719b64783390800c
Blue Mouse <durexp@protonmail.ch>
2023-10-08 05:11:03 +0100

    added support for categories

--- app/routes/blog.tsx
+++ app/routes/blog.tsx
@@ -36,17 +36,18 @@ export default function Posts() {
                   <div
                     className='absolute -inset-y-2.5 -inset-x-4 md:-inset-y-4 md:-inset-x-6 sm:rounded-2xl group-hover:bg-slate-50/70 dark:group-hover:bg-slate-800/50'>
                   </div>
-                  <svg viewBox='0 0 9 9'
-                    className='hidden absolute right-full mr-6 top-2 text-slate-200 dark:text-slate-600 md:mr-12 w-[calc(0.5rem+1px)] h-[calc(0.5rem+1px)] overflow-visible sm:block'>
-                    <circle cx='4.5' cy='4.5' r='4.5' stroke='currentColor' className='fill-white dark:fill-slate-800'
-                      strokeWidth='2'></circle>
+                  <svg viewBox='0 0 9 9' className='hidden absolute right-full mr-6 top-2 text-slate-200 dark:text-slate-600 md:mr-12 w-[calc(0.5rem+1px)] h-[calc(0.5rem+1px)] overflow-visible sm:block'>
+                    <circle cx='4.5' cy='4.5' r='4.5' stroke='currentColor' className='fill-white dark:fill-slate-800' strokeWidth='2' />
                   </svg>
                   <div className='relative'>
                     <h3 className='text-base font-semibold tracking-tight text-slate-900 dark:text-slate-200 pt-8 lg:pt-0'>
-                      {post.title}</h3>
-                    <div className='mt-2 mb-4 prose prose-slate prose-a:relative prose-a:z-10 dark:prose-dark line-clamp-2'>
-                      <p>{post.intro}</p>
-                    </div>
+                      {post.title}
+                    </h3>
+                    {post.intro && (
+                      <div className='mt-2 mb-4 prose prose-slate prose-a:relative prose-a:z-10 dark:prose-dark line-clamp-2'>
+                        <p>{post.intro}</p>
+                      </div>
+                    )}
                     <dl className='absolute left-0 top-0 lg:left-auto lg:right-full lg:mr-[calc(5rem+1px)]'>
                       <dt className='sr-only'>Date</dt>
                       <dd className='whitespace-nowrap text-sm leading-6 dark:text-slate-400'>
--- app/utils/getPosts.ts
+++ app/utils/getPosts.ts
@@ -5,12 +5,28 @@ import { marked } from 'marked'
 import _map from 'lodash/map'
 import _find from 'lodash/find'
 import _includes from 'lodash/includes'
+import _filter from 'lodash/filter'
+import _endsWith from 'lodash/endsWith'
 import _replace from 'lodash/replace'
+import _isEmpty from 'lodash/isEmpty'
+import _size from 'lodash/size'
 import { LoaderFunction } from '@remix-run/node'
+
+const renderer = new marked.Renderer()
+
+renderer.link = (href: string, title: string | null | undefined, text: string) => {
+  const url = new URL(href)
+  
+  if (url.hostname !== 'swetrix.com') {
+    url.searchParams.append('utm_source', 'swetrix.com')
+  }
+
+  return `<a href="${url.toString()}" referrerpolicy="strict-origin-when-cross-origin" target="_blank" rel="noopener noreferrer">${text}</a>`
+}
 
 export type PostMarkdownAttributes = {
   title: string
-  intro: string
+  intro?: string
   date: string
   author: string
   nickname: string
@@ -32,19 +48,58 @@ const findFilenameBySlug = (list: string[], handle: string) => {
   return _find(list, (item) => _includes(item, handle))
 }
 
-export const getFileNames = async (): Promise<string[]> => {
-  return fs.readdir(postsPath) as Promise<string[]>
+export const getSitemapFileNames = async (category?: string, infiniteRecursive?: boolean): Promise<any[]> => {
+  const allFiles = await fs.readdir(category ? path.join(postsPath, category) : postsPath) as string[]
+  const directories = _filter(allFiles, (file) => !_endsWith(file, '.md'))
+
+  if (_isEmpty(directories)) {
+    return allFiles
+  }
+
+  const files = _filter(allFiles, (file) => _endsWith(file, '.md'))
+
+  let filesInDirectories: any[] = []
+
+  for (let i = 0; i < _size(directories); ++i) {
+    const directory = directories[i]
+
+    // set to true for infinite depth directory look-up
+    const _files = await getSitemapFileNames(directory, infiniteRecursive)
+    filesInDirectories = [
+      ...filesInDirectories,
+      ..._map(_files, (file) => [directory, file]),
+    ]
+  }
+
+  return [
+    ...files,
+    ...filesInDirectories,
+  ]
+} 
+
+export const getFileNames = async (category?: string): Promise<string[]> => {
+  let files
+
+  if (category) {
+    files = await fs.readdir(path.join(postsPath, category)) as string[]
+  } else {
+    files = await fs.readdir(postsPath) as string[]
+  }
+
+  return _filter(files, (file) => _endsWith(file, '.md'))
 }
 
-export async function getPost(slug: string) {
-  const files = await getFileNames()
+export async function getPost(slug: string, category?: string) {
+  const files = await getFileNames(category)
   const filename = findFilenameBySlug(files, slug)
 
   if (!filename) {
     return null
   }
 
-  const filepath = path.join(postsPath, filename)
+  const filepath = category
+    ? path.join(postsPath, category, filename)
+    : path.join(postsPath, filename)
 
   const file = await fs.readFile(filepath)
   const { attributes, body }: IParseFontMatter = parseFrontMatter(file.toString())
@@ -52,7 +107,7 @@ export async function getPost(slug: string) {
   return {
     slug,
     title: attributes.title,
-    html: marked(body),
+    html: marked(body, { renderer }),
     hidden: attributes.hidden,
     intro: attributes.intro,
     date: attributes.date,
@@ -63,9 +118,10 @@ export async function getPost(slug: string) {
 
 export const blogLoader: LoaderFunction = async () => {
   const dir = await fs.readdir(postsPath)
+  const filtered = _filter(dir, (file) => _endsWith(file, '.md'))
 
   return Promise.all(
-    _map(dir, async filename => {
+    _map(filtered, async filename => {
       const file = await fs.readFile(
         path.join(postsPath, filename)
       )
