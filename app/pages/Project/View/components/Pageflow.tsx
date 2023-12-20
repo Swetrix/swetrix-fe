dd8c219d2aed6d89e5815faa0e15b3721fbb86cf
Blue Mouse <durexp@protonmail.ch>
2023-12-20 15:05:31 +0000

    translations

--- app/pages/Project/View/components/Pageflow.tsx
+++ app/pages/Project/View/components/Pageflow.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
-import { NewspaperIcon } from '@heroicons/react/24/outline'
+import { NewspaperIcon, CursorArrowRaysIcon } from '@heroicons/react/24/outline'
 import _map from 'lodash/map'
+import { Trans, useTranslation } from 'react-i18next'
 
 interface IPageflow {
   pages: {
@@ -11,40 +12,59 @@ interface IPageflow {
 }
 
 export const Pageflow = ({ pages }: IPageflow) => {
+  const { t, i18n: { language } } = useTranslation('common')
+
   return (
     <div className='flow-root'>
       <ul className='-mb-8'>
-        {_map(pages, ({ value, created }, index) => (
-          <li key={`${value}${created}`}>
-            <div className='relative pb-8'>
-              {index !== pages.length - 1 ? (
-                <span className='absolute left-4 top-4 -ml-px h-full w-0.5 bg-gray-200' aria-hidden='true' />
-              ) : null}
-              <div className='relative flex space-x-3'>
-                <div>
-                  <span
-                    className='h-8 w-8 rounded-full flex items-center justify-center ring-8 ring-white bg-gray-400'
-                  >
-                    <NewspaperIcon className='h-5 w-5 text-white' aria-hidden='true' />
-                  </span>
-                </div>
-                <div className='flex min-w-0 flex-1 justify-between space-x-4 pt-1.5'>
+        {_map(pages, ({ value, created, type }, index) => {
+          const displayCreated = new Date(created).toLocaleString(language)
+
+          return (
+            <li key={`${value}${created}`}>
+              <div className='relative pb-8'>
+                {index !== pages.length - 1 ? (
+                  <span className='absolute left-4 top-4 -ml-px h-full w-0.5 bg-gray-200' aria-hidden='true' />
+                ) : null}
+                <div className='relative flex space-x-3'>
                   <div>
-                    <p className='text-sm text-gray-500'>
-                      Pageview
-                      <span className='font-medium text-gray-900'>
-                        {value}
-                      </span>
-                    </p>
+                    <span
+                      className='h-8 w-8 rounded-full flex items-center justify-center ring-8 ring-white bg-gray-400'
+                    >
+                      {type === 'pageview' && (
+                        <NewspaperIcon className='h-5 w-5 text-white' aria-hidden='true' />
+                      )}
+                      {type === 'event' && (
+                        <CursorArrowRaysIcon className='h-5 w-5 text-white' aria-hidden='true' />
+                      )}
+                    </span>
                   </div>
-                  <div className='whitespace-nowrap text-right text-sm text-gray-500'>
-                    <time dateTime={created}>{created}</time>
+                  <div className='flex min-w-0 flex-1 justify-between space-x-4 pt-1.5'>
+                    <div>
+                      <p className='text-sm text-gray-500'>
+                        <Trans
+                          t={t}
+                          i18nKey={type === 'pageview' ? 'project.pageviewX' : 'project.eventX'}
+                          components={{
+                            value: <span className='font-medium text-gray-900' />,
+                          }}
+                          values={{
+                            x: value,
+                          }}
+                        />
+                      </p>
+                    </div>
+                    <div className='whitespace-nowrap text-right text-sm text-gray-500'>
+                      <time dateTime={created}>
+                        {displayCreated}
+                      </time>
+                    </div>
                   </div>
                 </div>
               </div>
-            </div>
-          </li>
-        ))}
+            </li>
+          )
+        })}
       </ul>
     </div>
   )
